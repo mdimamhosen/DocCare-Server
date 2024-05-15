2a0fc966e4a97b900ba97c70ef822ec67bd9780c
mdimamhosen <imamhosen9426224@gmail.com>
2024-05-15 18:27:07 +0600

    updating .gitignore file

--- index.js
+++ index.js
@@ -114,6 +114,7 @@ async function run() {
 
       res.json(finalResult);
     });
+
     app.patch("/updateServiceState/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
