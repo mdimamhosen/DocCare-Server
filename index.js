e2562b8e2c94ebd121d9aa0f1e23225b4cd47f31
mdimamhosen <imamhosen9426224@gmail.com>
2024-05-15 18:35:31 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -199,14 +199,14 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("You successfully connected to MongoDB!");
   } finally {
-    //
+    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World...!");
 });
 
 app.listen(PORT, () => {
